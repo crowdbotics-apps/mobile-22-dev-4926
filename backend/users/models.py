08243e279f3a9ce711319e40ec259dd37cccc8e9
Crowdbotics <team@crowdbotics.com>
2020-05-22 06:41:53 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -7,8 +7,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class User(AbstractUser):
     name = models.CharField(null=True, blank=True, max_length=255,)
-    jhgkjhgkjhgkjhgkjg = models.BigIntegerField(null=True, blank=True,)
-    jzhgfhsfkjghrf = models.BigIntegerField(null=True, blank=True,)
     jhgsafakd = models.BigIntegerField(null=True, blank=True,)
     jhghjgfw = models.ManyToManyField(
         "home.HomePage", blank=True, related_name="user_jhghjgfw",
